cf0e7beec246f19526358e41c705cba18567b0a7
Emmanuel Nsiah <ensiah193@gmail.com>
2023-03-25 16:23:19 -0600

    Completed Part 4 Challenge 16

--- part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
+++ part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
@@ -2,14 +2,26 @@
 public class MainProgram {
 
     public static void main(String[] args) {
-        PaymentCard card = new PaymentCard(50);
-        System.out.println(card);
+        PaymentCard paulsCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
+        paulsCard.eatHeartily();
+        mattsCard.eatAffordably();
         
-        card.eatAffordably();
-        System.out.println(card);
-        
-        card.eatHeartily();
-        System.out.println(card);
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
 
+        paulsCard.addMoney(20);
+        mattsCard.eatHeartily();
+
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
+        
+        paulsCard.eatAffordably();
+        paulsCard.eatAffordably();
+        
+        mattsCard.addMoney(50);
+        
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
     }
 }
