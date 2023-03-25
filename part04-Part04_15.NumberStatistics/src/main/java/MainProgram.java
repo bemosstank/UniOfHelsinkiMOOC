d99089998cec3a8274f4fa5efe58348dd6ec4b46
Emmanuel Nsiah <ensiah193@gmail.com>
2023-03-25 11:06:52 -0600

    Completed Part 4 challenge 15

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -5,12 +5,32 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // you can write test code here
-        // however, remove all unnecessary code when doing the final parts of the exercise
+        Statistics sum = new Statistics();
+        Statistics even = new Statistics();
+        Statistics odd = new Statistics();
 
-        // In order for the tests to work, the objects must be created in the
-        // correct order in the main program. First the object that tracks the total
-        // sum, secondly the object that tracks the sum of even numbers, 
-        // and lastly the one that tracks the sum of odd numbers!
+        System.out.println("Enter numbers:");
+        while (true) {
+            int userInput = scanner.nextInt();
+            if (userInput == -1) {
+                System.out.println("sum: " + sum.sum());
+                System.out.println("Sum of even numbers: " + even.sum());
+                System.out.println("Sum of odd numbers:"+ odd.sum());
+                   break;
+            }else {
+                sum.addNumber(userInput);
+                
+                if(userInput % 2 == 0){
+                    even.addNumber(userInput);
+                }else{
+                    odd.addNumber(userInput);
+                }
+            }
+        }
     }
-}
+}  
+    
+
+   
+
+   
