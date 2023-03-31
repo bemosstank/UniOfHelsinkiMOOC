9a7136a8b6b303fec7e2203658659b7d11bc7c86
Emmanuel Nsiah <ensiah193@gmail.com>
2023-03-31 12:20:30 -0600

    Completed part 4 exercise 21

--- part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -1,10 +1,19 @@
-
 import java.util.Scanner;
 
 public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String input="";
+        int counter =0;
+        while(true){
+            
+            if(input.equals("end")){
+                break;
+            }
+            input = scanner.nextLine();
+            counter = counter+1;
+        }
+        System.out.println(--counter);
     }
 }
