933f4e7185f6c1325cde10dcab8f69101d0cfdfa
Emmanuel Nsiah <ensiah193@gmail.com>
2023-03-31 14:58:10 -0600

    Completed part 4, exercise 24

--- part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
+++ part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -1,10 +1,25 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PrintingAFile {
 
     public static void main(String[] args) {
+        printFile();
+    }
 
+    public static void printFile() {
+        try {Scanner scan = new Scanner(Paths.get("data.txt"));
+        while(scan.hasNextLine()){
+            String row = scan.nextLine();
+            System.out.println(row);
+        }
+        } catch (IOException ex) {
+            System.out.println("Error " + ex);
+        }
+        
     }
 }
