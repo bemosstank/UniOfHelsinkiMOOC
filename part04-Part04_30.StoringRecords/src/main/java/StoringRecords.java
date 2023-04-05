a94e53d7f819d71a17dfd4dc544e7f02622c6857
Emmanuel Nsiah <ensiah193@gmail.com>
2023-04-05 12:56:13 -0600

    Completed part 4, exercise 30

--- part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
+++ part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
@@ -1,4 +1,5 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -14,17 +15,32 @@ public class StoringRecords {
         ArrayList<Person> records = readRecordsFromFile(file);
         System.out.println("Persons: " + records.size());
         System.out.println("Persons:");
+
         for (Person person : records) {
             System.out.println(person);
 
         }
+        readRecordsFromFile(file);
     }
 
     public static ArrayList<Person> readRecordsFromFile(String file) {
         ArrayList<Person> persons = new ArrayList<>();
 
-        // Write here the code for reading from file
-        // and printing the read records
+        try (Scanner fileScan = new Scanner(Paths.get(file))) {
+            String[] splitRow;
+            String name;
+            int age;
+            while (fileScan.hasNextLine()) {
+                String row = fileScan.nextLine();
+
+                splitRow = row.split(",");
+                name = splitRow[0];
+                age = Integer.valueOf(splitRow[1]);
+                persons.add(new Person(name, age));
+            }
+        } catch (IOException exe) {
+            System.out.println("Error: " + exe.getMessage());
+        }
         return persons;
 
     }
