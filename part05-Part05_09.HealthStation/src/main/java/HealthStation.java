7b77e42d4eaa96c12ff24f1e4a568f087de0b851
Emmanuel Nsiah <ensiah193@gmail.com>
2023-04-08 12:29:28 -0600

    Completed part 5, exercise 9

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,21 @@
 
 public class HealthStation {
 
+    private int weighings;
 
     public int weigh(Person person) {
-        // return the weight of the person passed as the parameter
-        return -1;
+        weighings = weighings + 1;
+        return person.getWeight();
     }
 
+    public void feed(Person person) {
+        int currentWeight = person.getWeight();
+        currentWeight = currentWeight + 1;
+
+        person.setWeight(currentWeight);
+    }
+
+    public int weighings() {
+        return weighings;
+    }
 }
