cf1117266d97d65c295fa8185aaa72730bf4626b
Emmanuel Nsiah <ensiah193@gmail.com>
2023-04-10 17:07:49 -0600

    Completed part 5, exercise 10

--- part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -1,30 +1,66 @@
-
 public class PaymentTerminal {
-
 
     private double money;  // amount of cash
     private int affordableMeals; // number of sold affordable meals
-    private int heartyMeals;  // number of sold hearty meals
-
+    private int heartyMeals;// number of sold hearty meals
+    private static final double AFFORDABLE_PRICE = 2.50;
+    private static final double HEARTY_PRICE = 4.30;
 
     public PaymentTerminal() {
         // register initially has 1000 euros of money
+        this.money = 1000;
+    }
+    public void addMoneyToCard(PaymentCard card, double sum){
+       if(sum>0){
+           card.addMoney(sum);
+           this.money = this.money +sum;
+       }
+        
+    }
+    
+    public boolean eatAffordably(PaymentCard card){
+        if(card.balance() >= AFFORDABLE_PRICE){
+            this.affordableMeals = this.affordableMeals+1;
+            return card.takeMoney(AFFORDABLE_PRICE);
+        }
+        return false;
+    }
+    
+    public boolean eatHeartily(PaymentCard card){
+        if(card.balance() >= HEARTY_PRICE){
+            this.heartyMeals = this.heartyMeals+1;
+            return card.takeMoney(HEARTY_PRICE);
+        }
+        return false;
     }
 
     public double eatAffordably(double payment) {
         // an affordable meal costs 2.50 euros
-        // increase the amount of cash by the price of an affordable mean and return the change
+        // increase the amount of cash by the price of an affordable meal and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if (AFFORDABLE_PRICE > payment) {
+            System.out.println("No sale");
+            return payment;
+        }
+        this.money = this.AFFORDABLE_PRICE + this.money;
+        affordableMeals = affordableMeals+1;
+        return payment - AFFORDABLE_PRICE;
+
     }
 
     public double eatHeartily(double payment) {
         // a hearty meal costs 4.30 euros
         // increase the amount of cash by the price of a hearty mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+       if (HEARTY_PRICE > payment){
+          
+           return payment;
+       }
+       this.money = this.HEARTY_PRICE + this.money;
+        this.heartyMeals = this.heartyMeals+1;
+        
+        return payment - this.HEARTY_PRICE;
     }
-
 
     @Override
     public String toString() {
